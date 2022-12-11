ce5205578e561646b4486802a12a2b92c2fcc1cc
Gachara <bgachara@gmail.com>
2022-12-11 14:52:01 +0300

    c_language genesis

---
+++ first.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Hello World!\n");
+    printf("Hello World! This message is brought to you by Zig\n");
+}
---
+++ reference.md
@@ -0,0 +1,5 @@
+# C reference
+
+`This is my personal C programming language reference`
+
+## General Notes about the language
